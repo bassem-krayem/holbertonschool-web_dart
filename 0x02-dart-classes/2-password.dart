e2b949f093891765454595bb41c22a1ce84ac8ec
bassem <104699079+bassem-krayem@users.noreply.github.com>
2026-04-15 10:56:11 +0000

    feature: dart-classes project task:2

---
+++ 0x02-dart-classes/2-password.dart
@@ -0,0 +1,22 @@
+class Password {
+  String _password = "";
+
+  set password(String value) {
+    _password = value;
+  }
+
+  String get password => _password;
+
+  bool isValid() {
+    if (_password.length < 8 || _password.length > 16) return false;
+    if (!_password.contains(RegExp(r'[A-Z]'))) return false;
+    if (!_password.contains(RegExp(r'[a-z]'))) return false;
+    if (!_password.contains(RegExp(r'[0-9]'))) return false;
+    return true;
+  }
+
+  @override
+  String toString() {
+    return "Your Password is: ${_password}";
+  }
+}
